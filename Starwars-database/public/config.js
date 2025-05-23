ed8762885e9fe5a211778d0a487d49cbfab92cc0
Spencer Knight <SpencerKnight2019@gmail.com>
2025-05-22 19:06:31 -0600

    save...

---
+++ Starwars-database/public/config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAhBg7-t86vOq9TXwtXg49hV96UJNQ45CE",
+  authDomain: "starwars-database-48052.firebaseapp.com",
+  projectId: "starwars-database-48052",
+  storageBucket: "starwars-database-48052.firebasestorage.app",
+  messagingSenderId: "59591469227",
+  appId: "1:59591469227:web:a1e1100b2a77d768afafee",
+  measurementId: "G-BCG9SR3G8W"
+};
+
+  
